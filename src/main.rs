e4641c9a3251d95a06c9079af189cbd727433dc2
Nao Minami <south37777@gmail.com>
2025-12-30 21:13:05 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(link_section = ".multiboot")]
+#[unsafe(no_mangle)]
+pub static MULTIBOOT_HEADER: [u32; 3] = [
+    0x1BADB002, // magic
+    0x00000003, // flags
+    0xE4524FFB, // checksum = -(magic + flags)
+];
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start(_multiboot_info: u32) -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
