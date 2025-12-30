a78cefcb1f23b561034e6ad23e7b5c842956a123
Nao Minami <south37777@gmail.com>
2025-12-30 22:30:54 +0900

    Set up build pipeline

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,8 @@
 
 use core::panic::PanicInfo;
 
-#[unsafe(link_section = ".multiboot")]
 #[unsafe(no_mangle)]
-pub static MULTIBOOT_HEADER: [u32; 3] = [
-    0x1BADB002, // magic
-    0x00000003, // flags
-    0xE4524FFB, // checksum = -(magic + flags)
-];
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start(_multiboot_info: u32) -> ! {
+pub extern "C" fn kmain() -> ! {
     loop {}
 }
 
