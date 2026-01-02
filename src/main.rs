6b29aeb8545d0c1c8a96260bb37246f07332cca7
Nao Minami <south37777@gmail.com>
2026-01-02 20:35:43 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -35,20 +35,14 @@ pub extern "C" fn kmain() -> ! {
         kernel_range().1
     );
 
-    let mut kernel = Kernel::new();
-    kernel.allocator.init(kernel_range().1, p2v(PHYS_MEM));
+    let mut allocator = Allocator::new();
+    allocator.init(kernel_range().1, p2v(PHYS_MEM));
 
     // Debug
-    debug_freelist(&mut kernel.allocator);
+    debug_freelist(&mut allocator);
 
-    // Kernel virtual memory
-    vm::init(&mut kernel.allocator);
+    vm::init(&mut allocator);
     uart_println!("Page table loaded");
-
-    // Test paging
-    unsafe {
-        test_paging();
-    }
 
     gdt::init();
     uart_println!("GDT loaded");
@@ -62,7 +56,7 @@ pub extern "C" fn kmain() -> ! {
     trap::init();
     uart_println!("Traps initialized");
 
-    proc::init_process(&mut kernel.allocator);
+    proc::init_process(&mut allocator);
     uart_println!("Init process initialized");
 
     // Enable interrupts
@@ -73,7 +67,7 @@ pub extern "C" fn kmain() -> ! {
     proc::scheduler();
 
     // Debug
-    debug_freelist(&mut kernel.allocator);
+    debug_freelist(&mut allocator);
     loop {
         unsafe {
             core::arch::asm!("hlt");
@@ -84,48 +78,6 @@ pub extern "C" fn kmain() -> ! {
 fn debug_freelist(allocator: &mut Allocator) {
     let addr = allocator.freelist as *const u8 as usize;
     uart_println!("freelist: 0x{:x}", addr);
-}
-
-struct Kernel {
-    allocator: Allocator,
-}
-
-impl Kernel {
-    fn new() -> Self {
-        Self {
-            allocator: Allocator::new(),
-        }
-    }
-}
-
-unsafe fn test_paging() {
-    let virt_addr1 = KERNBASE as *mut u32;
-
-    uart_println!("Testing paging...");
-
-    // Save original value
-    let original_value = unsafe { *virt_addr1 };
-    uart_println!("Original value at KERNBASE: 0x{:x}", original_value);
-
-    // Write to KERNBASE
-    uart_println!("Writing 0xDEADBEEF to KERNBASE");
-    unsafe { *virt_addr1 = 0xDEADBEEF };
-
-    // Read from address 0 using assembly to avoid Rust null pointer check/panic
-    let val: u32;
-    unsafe {
-        core::arch::asm!("mov {0:e}, [0]", out(reg) val);
-    }
-    uart_println!("Read 0x{:x} from address 0", val);
-
-    if val == 0xDEADBEEF {
-        uart_println!("Paging test passed!");
-    } else {
-        uart_println!("Paging test failed!");
-    }
-
-    // Restore
-    unsafe { *virt_addr1 = original_value };
 }
 
 #[panic_handler]
